4edb1dec0b134ee45eae0a2440a9cca3efce2a79
Kosenkov <9588434@gmail.com>
2022-08-19 19:04:16 +0300

    Task 2 done

--- task-2/Program.cs
+++ task-2/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ввведите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввведите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x>y) Console.Write($"Число {x} больше числа {y}");
+else if (y>x) Console.Write($"Число {y} больше числа {x}");
+else Console.Write("Числа равны");
