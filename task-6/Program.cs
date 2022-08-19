6a1a3d3469e4842ce4fc1e6e8b77177782d17377
Kosenkov <9588434@gmail.com>
2022-08-19 19:20:41 +0300

    Task 6 done

--- task-6/Program.cs
+++ task-6/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ввведите число для проверки: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x % 2 == 0) Console.Write($"{x} является четным число");
+else Console.Write($"{x} является не четным число");
