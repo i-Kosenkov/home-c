7e5fc72249f8d04015b82cfa82b14be928957801
Kosenkov <9588434@gmail.com>
2022-08-19 19:34:44 +0300

    Task 8 done

--- task-8/Program.cs
+++ task-8/Program.cs
@@ -1,2 +1,6 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ввведите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= x; i++)
+{
+    if (i % 2 == 0) Console.WriteLine(i);
+}
