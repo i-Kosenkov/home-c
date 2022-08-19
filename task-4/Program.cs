399fcc4091ffe80ee3cf3e9006126100e0e4dab6
Kosenkov <9588434@gmail.com>
2022-08-19 19:14:07 +0300

    Task 4 done

--- task-4/Program.cs
+++ task-4/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Ввведите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввведите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввведите третье число: ");
+int z = Convert.ToInt32(Console.ReadLine());
+int max = x;
+if (y > max) max = y;
+if (z > max) max = z;
+Console.Write($"Максимальное число {max}");
