901ae6abc31d1f4c37616b36620fc131f60cfa9e
Kosenkov <9588434@gmail.com>
2022-08-23 13:44:18 +0300

    Add task 15 - done

---
+++ task-15/Program.cs
@@ -0,0 +1,19 @@
+﻿string WeekDays(int NumberDay)
+{
+    switch (NumberDay)
+    {
+        case 1: return "понедельник";
+        case 2: return "вторник";
+        case 3: return "среда";
+        case 4: return "четверг";
+        case 5: return "пятница";
+        case 6: return "суббота";
+        case 7: return "воскресенье";
+        default: return "неверное значение";
+    }
+}
+
+Console.Write ("Введите цифрой день недели: ");
+int x = Convert.ToInt32(Console.ReadLine());
+string Day = WeekDays(x);
+Console.Write ($"День недели: {Day}");
