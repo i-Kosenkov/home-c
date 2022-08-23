dff4e1ed8caa6d2558ff2427aba342687d000ce1
Kosenkov <9588434@gmail.com>
2022-08-23 13:50:49 +0300

    Add yes no for question

--- task-15/Program.cs
+++ task-15/Program.cs
@@ -2,18 +2,18 @@
 {
     switch (NumberDay)
     {
-        case 1: return "понедельник";
-        case 2: return "вторник";
-        case 3: return "среда";
-        case 4: return "четверг";
-        case 5: return "пятница";
-        case 6: return "суббота";
-        case 7: return "воскресенье";
-        default: return "неверное значение";
+        case 1: return "нет - понедельник";
+        case 2: return "нет - вторник";
+        case 3: return "нет - среда";
+        case 4: return "нет - четверг";
+        case 5: return "нет - пятница";
+        case 6: return "да - суббота";
+        case 7: return "да - воскресенье";
+        default: return "Ввели неверное числовое значение";
     }
 }
 
-Console.Write ("Введите цифрой день недели: ");
+Console.Write("Введите день недели числом: ");
 int x = Convert.ToInt32(Console.ReadLine());
 string Day = WeekDays(x);
-Console.Write ($"День недели: {Day}");
+Console.Write($"День недели выходной? {Day}");
