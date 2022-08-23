f8a4b33f8192115c1ec218be385f413126672420
Kosenkov <9588434@gmail.com>
2022-08-23 11:30:21 +0300

    Add function: Middle Number

--- task-10/Program.cs
+++ task-10/Program.cs
@@ -4,17 +4,23 @@
     return NumberX;
 }
 
+int MiddleNumber(int NumberZ)
+{
+    NumberZ = (NumberZ % 100) / 10;
+    return NumberZ;
+}
+
 Console.Write("Подобрать случайное число (да/нет)? ");
 string YesNo = Console.ReadLine();
 if (YesNo.ToLower() == "да")
 {
     int x = RandomNumber();
     Console.WriteLine(x);
-    Console.WriteLine($"Вторая цифра числа: {(x % 100)/10}");
+    Console.WriteLine($"Вторая цифра числа: {MiddleNumber(x)}");
 }
 else
 {
     Console.Write("Введите трехзначное число: ");
     int x = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine($"Вторая цифра числа: {(x % 100)/10}");
+    Console.WriteLine($"Вторая цифра числа: {MiddleNumber(x)}");
 }
