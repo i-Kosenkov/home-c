260cd2cdd16623e290a12f9c51cad9278dfb3e4d
Kosenkov <9588434@gmail.com>
2022-08-23 11:22:13 +0300

    Task 10 done

--- task-10/Program.cs
+++ task-10/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿int RandomNumber()
+{
+    int NumberX = new Random().Next(100, 1000);
+    return NumberX;
+}
+
+Console.Write("Подобрать случайное число (да/нет)? ");
+string YesNo = Console.ReadLine();
+if (YesNo.ToLower() == "да")
+{
+    int x = RandomNumber();
+    Console.WriteLine(x);
+    Console.WriteLine($"Вторая цифра числа: {(x % 100)/10}");
+}
+else
+{
+    Console.Write("Введите трехзначное число: ");
+    int x = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Вторая цифра числа: {(x % 100)/10}");
+}
