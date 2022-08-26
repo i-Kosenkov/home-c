3a2492b06a23641cd372fd094c76c566832ce67f
Kosenkov <9588434@gmail.com>
2022-08-26 11:08:49 +0300

    Delete old folders

--- task-6/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿Console.Write("Ввведите число для проверки: ");
-int x = Convert.ToInt32(Console.ReadLine());
-if (x % 2 == 0) Console.Write($"{x} является четным число");
-else Console.Write($"{x} является не четным число");
--- task-8/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.Write("Ввведите число: ");
-int x = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i <= x; i++)
-{
-    if (i % 2 == 0) Console.WriteLine(i);
-}
