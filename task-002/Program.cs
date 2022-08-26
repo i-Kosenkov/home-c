af0c45897f8593afa3a5a71ba2586fb77c0a048d
Kosenkov <9588434@gmail.com>
2022-08-26 11:02:42 +0300

    Add Task 2

---
+++ task-002/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Ввведите первое число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ввведите второе число: ");
+int y = Convert.ToInt32(Console.ReadLine());
+if (x > y) Console.Write($"Число {x} больше числа {y}");
+else if (y > x) Console.Write($"Число {y} больше числа {x}");
+else Console.Write("Числа равны");
