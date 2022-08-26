1f5adc89a84eb6577171d845a757e0d6b37e808b
Kosenkov <9588434@gmail.com>
2022-08-26 12:30:51 +0300

    Add task 23

---
+++ task-023/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите значение N: ");
+bool isNum_N = int.TryParse(Console.ReadLine(), out int N);
+
+if (!isNum_N || N < 1)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+void GetCube(int X)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        Console.Write($"{Math.Pow(i, 3)} ");
+    }
+}
+
+GetCube(N);
