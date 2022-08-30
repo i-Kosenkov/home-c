427d1ccc89d46d550f8afd3b598d1a30f46b8aa1
Kosenkov <9588434@gmail.com>
2022-08-30 14:17:28 +0300

    Add task 29

---
+++ task-029/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Write("Введите объем массива: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+
+if (!isNum_x || x < 1)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+//Заполняем массив, ЛЮБЫМИ значениями!
+string[] GetArray(int lenght)
+{
+    string[] array = new string[lenght];
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите {i} элемент массива:");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+//Вывод данных из массива
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+PrintArray(GetArray(x));
