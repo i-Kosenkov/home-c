d96756344b46a5dd1d81fc3c1865c4f164a7ad54
Kosenkov <9588434@gmail.com>
2022-08-30 13:59:46 +0300

    Add task 27

---
+++ task-27/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите значение x: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+
+if (!isNum_x || x < 0)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+
+int GetSum(int a)
+{
+    int result = 0;
+    for (int i = 0; a > 0; i++)
+    {
+        result = result + a % 10;
+        a = a / 10;
+    }
+    return result;
+}
+
+Console.Write(GetSum(x));
