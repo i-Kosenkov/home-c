7f85dc5431466f2fe48cb73d514481457b159819
Kosenkov <9588434@gmail.com>
2022-08-30 13:49:17 +0300

    Add task 25

---
+++ task-25/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Введите значение x: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+
+Console.Write("Введите значение степени: ");
+bool isNum_y = int.TryParse(Console.ReadLine(), out int y);
+
+if (!isNum_x || !isNum_y || y < 0)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+int GetDegree (int a, int b)
+{
+    int result = a;
+    for (int i = 1; i < b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.Write(GetDegree(x,y));
