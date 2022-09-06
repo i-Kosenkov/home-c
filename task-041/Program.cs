a24ef0116eddf4861ac1d57b63f062b1bd767146
Kosenkov <9588434@gmail.com>
2022-09-06 14:15:46 +0300

    Task 43 done

---
+++ task-041/Program.cs
@@ -0,0 +1,45 @@
+﻿//Заполянем массив рандомными числами
+int[] GetRandomArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+    return array;
+}
+
+//Печать массива
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+//Проверка вводимого числа 
+Console.Write("Введите длину массива: ");
+bool isNum_x = int.TryParse(Console.ReadLine(), out int x);
+if (!isNum_x || x <= 0)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+//Считаем сколько положительных чисел в массиве
+int GetPositiveNumber(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) result++;
+    }
+    return result;
+}
+
+int[] randomArray = GetRandomArray(x);
+PrintArray(randomArray);
+Console.WriteLine();
+Console.WriteLine($"Положительных чисел: {GetPositiveNumber(randomArray)}");
+
---
+++ task-043/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+//y = k1 * x + b1, y = k2 * x + b2; 
+//значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Write("Введите b1: ");
+bool isNum_b1 = double.TryParse(Console.ReadLine(), out double b1);
+Console.Write("Введите k1: ");
+bool isNum_k1 = double.TryParse(Console.ReadLine(), out double k1);
+Console.Write("Введите b2: ");
+bool isNum_b2 = double.TryParse(Console.ReadLine(), out double b2);
+Console.Write("Введите k2: ");
+bool isNum_k2 = double.TryParse(Console.ReadLine(), out double k2);
+
+if (!isNum_b1 || !isNum_k1 || !isNum_b2 || !isNum_k2)
+{
+    Console.WriteLine("Ошибка ввода");
+    return;
+}
+
+double[] GetConnectPoint(double x1, double y1, double x2, double y2)
+{
+double [] array = new double[2];
+array[0] = (x2 - x1) / (y1 - y2);
+array[1] = y2 * ((x2 - x1) / (y1 - y2)) + x2;
+return array;
+}
+
+double[] array = GetConnectPoint(b1,k1,b2,k2);
+
+Console.Write($"Точка пересечения прямых ({array[0]}, ");
+Console.Write($"{array[1]})");
