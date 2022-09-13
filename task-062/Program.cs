8b0e661871968d01e7bc8c2827798d42a2504cae
Kosenkov <9588434@gmail.com>
2022-09-13 22:20:26 +0300

    Task 62

---
+++ task-062/Program.cs
@@ -0,0 +1,30 @@
+﻿//Заполнить двумерный массив
+void GetNumberDoubleArray(int[,] array)
+{
+    int n = 1;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = n;
+            n++;
+        }
+    }
+}
+
+//печать двумерного массива
+void PrintDoubleArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] array = new int[4, 4];
+GetNumberDoubleArray(array);
+PrintDoubleArray(array);
