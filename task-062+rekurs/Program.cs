90dfc4bb8730634e3cdc527fd948c2a886ef3383
Kosenkov <9588434@gmail.com>
2022-09-15 14:37:28 +0300

    Допилил задание 62 с рекурсией

--- task-062+rekurs/Program.cs
+++ task-062+rekurs/Program.cs
@@ -1,18 +1,18 @@
 ﻿//Напишите программу, которая заполнит спирально массив 4 на 4. 
 
-int[,] array = new int[4, 4];
+int[,] array = new int[4, 6];
 int n = 11;
 FillArray(0, 0);
 PrintDoubleArray(array);
 
 void FillArray(int row, int col)
 {
-    Console.WriteLine($"row = {row}");
-    Console.WriteLine($"col = {col}");
-    Console.WriteLine($"n = {n}");
-    Console.WriteLine($"X = {array[row, col]}");
+    // Console.WriteLine($"row = {row}");
+    // Console.WriteLine($"col = {col}");
+    // Console.WriteLine($"n = {n}");
+    // Console.WriteLine($"X = {array[row, col]}");
+    // Console.WriteLine();
 
-    Console.WriteLine();
     if (array[row, col] == 0)
     {
         array[row, col] = n;
@@ -34,10 +34,32 @@ void FillArray(int row, int col)
         }
         if (row > 0)
         {
-            FillArray(row - 1, col);
+            FillArrayUpRow(row - 1, col);
         }
     }
+}
 
+void FillArrayUpRow(int row, int col)
+{
+    // Console.WriteLine($"!row = {row}");
+    // Console.WriteLine($"!col = {col}");
+    // Console.WriteLine($"!n = {n}");
+    // Console.WriteLine($"!X = {array[row, col]}");
+    // Console.WriteLine();
+
+    if (array[row, col] == 0)
+    {
+        array[row, col] = n;
+        n++;
+        if (row > 0)
+        {
+            FillArrayUpRow(row - 1, col);
+        }
+        if (col < array.GetLength(1) - 1)
+        {
+            FillArray(row, col + 1);
+        }
+    }
 }
 
 //печать двумерного массива
